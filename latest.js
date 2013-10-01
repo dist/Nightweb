f313ff340a39d3cbd86a5a1ff45270bbd3b2daf7
oakes <zsoakes@gmail.com>
2013-10-01 19:19:49 -0400

    Update to 0.0.21

--- latest.js
+++ latest.js
@@ -1,1 +1,1 @@
-latest='https://github.com/oakes/Nightweb/releases/download/0.0.20/nightweb-desktop-0.0.20-standalone.jar';
+latest='https://github.com/oakes/Nightweb/releases/download/0.0.21/nightweb-desktop-0.0.21-standalone.jar';
