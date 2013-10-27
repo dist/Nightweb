1038af4e70d8291614fc3bb6e43e8a52b09bcee3
oakes <zsoakes@gmail.com>
2013-10-27 19:53:29 -0400

    Update links to 0.0.22

--- latest.js
+++ latest.js
@@ -1,1 +1,1 @@
-latest='https://github.com/oakes/Nightweb/releases/download/0.0.21/nightweb-desktop-0.0.21-standalone.jar';
+latest='https://github.com/oakes/Nightweb/releases/download/0.0.22/nightweb-desktop-0.0.22-standalone.jar';
