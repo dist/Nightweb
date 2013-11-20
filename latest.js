d8d506ac52ae80e6d07babc179f7d61f9fc6d68b
oakes <zsoakes@gmail.com>
2013-11-19 19:38:48 -0500

    Remove test APK and update links

--- latest.js
+++ latest.js
@@ -1,1 +1,1 @@
-latest='https://github.com/oakes/Nightweb/releases/download/0.0.22/nightweb-desktop-0.0.22-standalone.jar';
+latest='https://github.com/oakes/Nightweb/releases/download/0.0.23/nightweb-desktop-0.0.23-standalone.jar';
