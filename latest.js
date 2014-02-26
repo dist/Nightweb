af111e86deb5ce1ae3ce9e13ae704103bc056dee
oakes <zsoakes@gmail.com>
2014-02-26 18:10:59 -0500

    Update link

--- latest.js
+++ latest.js
@@ -1,1 +1,1 @@
-latest='https://github.com/oakes/Nightweb/releases/download/0.0.25/nightweb-desktop-0.0.25-standalone.jar';
+latest='https://github.com/oakes/Nightweb/releases/download/0.0.26/nightweb-desktop-0.0.26-standalone.jar';
